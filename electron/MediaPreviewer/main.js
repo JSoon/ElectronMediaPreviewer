17bd47cc41d61be42111a2611489ac956d6ca221
J.Soon <serdeemail@gmail.com>
2021-09-03 16:49:43 +0800

    预览窗口打开逻辑优化

--- electron/MediaPreviewer/main.js
+++ electron/MediaPreviewer/main.js
@@ -38,6 +38,11 @@ class MediaPreviewer {
       },
     });
 
+    previewWin.on('closed', () => {
+      this.window.destroy();
+      this.window = null;
+    });
+
     if (config.env === 'development') {
       previewWin.loadURL(config.url);
     } else {
@@ -66,13 +71,25 @@ class MediaPreviewer {
 const useMediaPreviewer = (mainWindow) => {
   const previewer = new MediaPreviewer();
 
-  ipcMain.on(IPC_CHANNELS.MEDIA_PREVIEW, async (e, data) => {
-    console.log('IPC_CHANNELS.MEDIA_PREVIEW', data);
+  async function onMediaPreview(e, data) {
+    console.log('IPC_CHANNELS.MEDIA_PREVIEW');
     previewer.init(data);
-  });
+  }
 
-  ipcMain.on(IPC_CHANNELS.MEDIA_PREVIEW_CLOSE, async (e) => {
+  async function onMediaPreviewClose(e) {
     console.log('IPC_CHANNELS.MEDIA_PREVIEW_CLOSE');
+  }
+
+  ipcMain.on(IPC_CHANNELS.MEDIA_PREVIEW, onMediaPreview);
+
+  ipcMain.on(IPC_CHANNELS.MEDIA_PREVIEW_CLOSE, onMediaPreviewClose);
+
+  mainWindow.on('close', (e) => {
+    console.log('主窗口要关闭了', mainWindow);
+    // 主窗口关闭前, 解除事件绑定, 防止主进程事件重复绑定
+    // NOTE: 该代码逻辑需要根据主窗口进行调整, 若主窗口点击关闭仅做隐藏处理, 则无需解除事件绑定
+    // ipcMain.off(IPC_CHANNELS.MEDIA_PREVIEW, onMediaPreview);
+    // ipcMain.off(IPC_CHANNELS.MEDIA_PREVIEW_CLOSE, onMediaPreviewClose);
   });
 };
 
--- src/typings/media.ts
+++ src/typings/media.ts
@@ -6,12 +6,17 @@ export enum EMediaType {
 export type TMedia = EMediaType.IMG | EMediaType.VIDEO;
 
 export interface IMediaItem {
+  // 媒体类型
   type: TMedia;
+  // 媒体地址
   url: string;
 }
 
 export interface IMediaData {
+  // 预览媒体项索引号
   index: number;
+  // 预览媒体项
   media: IMediaItem;
+  // 预览媒体列表
   mediaList: IMediaItem[];
 }
