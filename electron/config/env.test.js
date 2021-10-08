1fd1f401c894754742f9281d035e023fd104562a
J.Soon <serdeemail@gmail.com>
2021-10-08 16:27:09 +0800

    修复生产环境配置

--- electron/config/env.test.js
+++ electron/config/env.test.js
@@ -2,5 +2,5 @@ const path = require('path');
 
 module.exports = {
   env: 'production',
-  url: path.join(__dirname, '../MediaPreviewer/statics/index.html'),
+  url: path.join(__dirname, 'statics/index.html'),
 };
