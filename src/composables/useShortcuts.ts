42f2bdf4140bae714c0f41ea986b3bfca89e8158
J.Soon <serdeemail@gmail.com>
2021-11-10 17:22:44 +0800

    取消空格快捷键操作

--- src/composables/useShortcuts.ts
+++ src/composables/useShortcuts.ts
@@ -6,12 +6,12 @@ export default (): void => {
   const { turnToPrev, turnToNext } = useMediaData();
 
   // 空格键, 退出键: 关闭预览
-  document.addEventListener('keyup', (e) => {
-    if (e.code === 'Space' || e.code === 'Escape') {
-      e.preventDefault();
-      ipcRenderer.send(IPC_CHANNELS.MEDIA_PREVIEW_CLOSE);
-    }
-  });
+  // document.addEventListener('keyup', (e) => {
+  //   if (e.code === 'Space' || e.code === 'Escape') {
+  //     e.preventDefault();
+  //     ipcRenderer.send(IPC_CHANNELS.MEDIA_PREVIEW_CLOSE);
+  //   }
+  // });
 
   // 禁用刷新
   document.addEventListener('keydown', (e) => {
